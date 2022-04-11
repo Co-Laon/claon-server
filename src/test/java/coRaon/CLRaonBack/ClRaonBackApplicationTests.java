89d5aec01c7484dc14882e9ea952cfae750d9ebf
ChanghyukWoo <89950941+ChanghyukWoo@users.noreply.github.com>
2022-04-11 13:18:43 +0900

    init (#1)

---
+++ src/test/java/coRaon/CLRaonBack/ClRaonBackApplicationTests.java
@@ -0,0 +1,13 @@
+package coRaon.CLRaonBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClRaonBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
