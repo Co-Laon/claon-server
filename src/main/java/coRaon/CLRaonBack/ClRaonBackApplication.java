de3c078b752d8d195d386e769a2c7145d9d74fca
changhyukwoo <wlji123@naver.com>
2022-04-11 12:47:56 +0900

    init

---
+++ src/main/java/coRaon/CLRaonBack/ClRaonBackApplication.java
@@ -0,0 +1,12 @@
+package coRaon.CLRaonBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClRaonBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClRaonBackApplication.class, args);
+	}
+}
