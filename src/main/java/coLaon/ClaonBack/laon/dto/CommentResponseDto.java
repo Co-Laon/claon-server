98017a9984728e6e2b822d8363305b8caf90e6a2
KwangpyoKo <81270740+KwangpyoKo@users.noreply.github.com>
2022-06-20 13:12:26 +0900

    feat: Implement create comment (#58)
    * feat: Implement create comment
    
    * feat: Implement create comment
    
    * feat: Implement create comment
    
    * feat: Implement create comment
    
    * feat: Implement create comment
    
    Co-authored-by: unknown <kwangpyo_ko@tmax.co.kr>

---
+++ src/main/java/coLaon/ClaonBack/laon/dto/CommentResponseDto.java
@@ -0,0 +1,38 @@
+package coLaon.ClaonBack.laon.dto;
+
+import coLaon.ClaonBack.laon.domain.LaonComment;
+import lombok.Data;
+
+@Data
+public class CommentResponseDto {
+    private String commentId;
+    private String content;
+    private Boolean isDeleted;
+    private String parentcommentId;
+    private String laonId;
+
+    private CommentResponseDto(
+            String commentId,
+            String content,
+            Boolean isDeleted,
+            String parentcommentId,
+            String laonId
+    ) {
+        this.commentId = commentId;
+        this.content = content;
+        this.isDeleted = isDeleted;
+        this.parentcommentId = parentcommentId;
+        this.laonId = laonId;
+    }
+
+    public static CommentResponseDto from(LaonComment laonComment) {
+        return new CommentResponseDto(
+                laonComment.getId(),
+                laonComment.getContent(),
+                laonComment.getIsDeleted(),
+                laonComment.getParentComment()!= null ? laonComment.getParentComment().getId() : null,
+                laonComment.getLaon().getId()
+        );
+    }
+
+}
