ed22217a130ad459c6312552490e15203734dffc
unknown <kwangpyo_ko@tmax.co.kr>
2022-06-20 12:37:08 +0900

    feat: Implement create comment

--- src/main/java/coLaon/ClaonBack/laon/dto/CommentRequestDto.java
+++ src/main/java/coLaon/ClaonBack/laon/dto/CommentRequestDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -11,6 +12,7 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentRequestDto {
+    @NotBlank(message = "댓글을 입력하세요")
     @Size(min = 1, max = 255, message = "댓글 최대 글자수는 255자입니다")
     private String content;
     private String parentCommentId;
