ab6846b4d3692c1e94c621f12908e7c9bcb357d1
minas <98951922+MinasKim@users.noreply.github.com>
2022-08-01 02:57:53 +0900

    fix : modify to create multiple reviews for one center test : implement a failure test for a center where reviews already exist

--- src/main/java/coLaon/ClaonBack/post/repository/ReviewRepository.java
+++ src/main/java/coLaon/ClaonBack/post/repository/ReviewRepository.java
@@ -2,6 +2,8 @@ package coLaon.ClaonBack.post.repository;
 
 import coLaon.ClaonBack.center.domain.CenterReview;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +11,9 @@ import java.util.Optional;
 @Repository
 public interface ReviewRepository extends JpaRepository<CenterReview, String> {
     Optional<CenterReview> findByIdAndIsDeletedFalse(String id);
+
+    @Query(value = "SELECT * "
+            + "FROM TB_CENTER_REVIEW AS r "
+            + "WHERE r.user_id = :userId AND r.center_id = :centerId", nativeQuery = true)
+    Optional<CenterReview> findByUserIdAndCenterId(@Param("userId") String userId, @Param("centerId") String centerId);
 }
