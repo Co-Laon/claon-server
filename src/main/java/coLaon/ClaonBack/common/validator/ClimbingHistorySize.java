0c6f4b37d8f86e373c7d44d39221fc0f20f90d5e
Minsoo <als95.dev@gmail.com>
2022-08-01 10:21:20 +0900

    refac: add climbing history size validator for create post

---
+++ src/main/java/coLaon/ClaonBack/common/validator/ClimbingHistorySize.java
@@ -0,0 +1,14 @@
+package coLaon.ClaonBack.common.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Constraint(validatedBy = ClimbingHistorySizeValidator.class)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ClimbingHistorySize {
+    String message();
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
