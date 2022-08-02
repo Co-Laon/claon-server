7219fb603c7b1b7c946e9ace386df7f26111ebb7
Minsoo <37828653+als95@users.noreply.github.com>
2022-08-02 14:56:02 +0900

    refac: add climbing history size validator for create post (#171)

--- src/main/java/coLaon/ClaonBack/post/dto/PostCreateRequestDto.java
+++ src/main/java/coLaon/ClaonBack/post/dto/PostCreateRequestDto.java
@@ -1,5 +1,6 @@
 package coLaon.ClaonBack.post.dto;
 
+import coLaon.ClaonBack.common.validator.ClimbingHistorySize;
 import coLaon.ClaonBack.common.validator.PostContentsSize;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,6 +15,7 @@ import java.util.List;
 public class PostCreateRequestDto {
     @NotBlank(message = "암장 정보를 입력해주세요.")
     private String centerId;
+    @ClimbingHistorySize(message = "1개 이상 10개 이하의 등반 기록을 입력 가능합니다.")
     private List<ClimbingHistoryRequestDto> climbingHistories;
     private String content;
     @PostContentsSize(message = "이미지 혹은 동영상 1개 이상 10개 이하로 업로드해야 합니다.")
