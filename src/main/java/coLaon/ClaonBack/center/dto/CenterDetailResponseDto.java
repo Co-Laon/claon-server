a82217a77ea34ef248ff4fb8e4611a6bef3917b0
ChanghyukWoo <89950941+ChanghyukWoo@users.noreply.github.com>
2022-08-02 23:21:03 +0900

    feat: center find (#173)

---
+++ src/main/java/coLaon/ClaonBack/center/dto/CenterDetailResponseDto.java
@@ -0,0 +1,99 @@
+package coLaon.ClaonBack.center.dto;
+
+import coLaon.ClaonBack.center.domain.Center;
+import coLaon.ClaonBack.center.domain.HoldInfo;
+import lombok.Data;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Data
+public class CenterDetailResponseDto {
+    private String id;
+    private String name;
+    private String address;
+    private String tel;
+    private String webUrl;
+    private String instagramUrl;
+    private String youtubeUrl;
+    private List<CenterImgDto> imgList;
+    private List<OperatingTimeDto> operatingTimeList;
+    private String facilities;
+    private List<ChargeDto> chargeList;
+    private String chargeImg;
+    private List<HoldInfoResponseDto> holdInfoList;
+    private String holdInfoImg;
+    private List<SectorInfoDto> sectorInfoList;
+    private Boolean isBookmarked;
+    private Integer postCount;
+    private Integer reviewCount;
+
+    private CenterDetailResponseDto(
+            String id,
+            String name,
+            String address,
+            String tel,
+            String webUrl,
+            String instagramUrl,
+            String youtubeUrl,
+            List<CenterImgDto> imgList,
+            List<OperatingTimeDto> operatingTimeList,
+            String facilities,
+            List<ChargeDto> chargeList,
+            String chargeImg,
+            List<HoldInfoResponseDto> holdInfoList,
+            String holdInfoImg,
+            List<SectorInfoDto> sectorInfoList,
+            Boolean isBookmarked,
+            Integer postCount,
+            Integer reviewCount
+    ) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.tel = tel;
+        this.webUrl = webUrl;
+        this.instagramUrl = instagramUrl;
+        this.youtubeUrl = youtubeUrl;
+        this.imgList = imgList;
+        this.operatingTimeList = operatingTimeList;
+        this.facilities = facilities;
+        this.chargeList = chargeList;
+        this.chargeImg = chargeImg;
+        this.holdInfoList = holdInfoList;
+        this.holdInfoImg = holdInfoImg;
+        this.sectorInfoList = sectorInfoList;
+        this.isBookmarked = isBookmarked;
+        this.postCount = postCount;
+        this.reviewCount = reviewCount;
+    }
+
+    public static CenterDetailResponseDto from(
+            Center center,
+            List<HoldInfo> holdInfoList,
+            Boolean isBookmarked,
+            Integer postCount,
+            Integer reviewCount
+    ) {
+        return new CenterDetailResponseDto(
+                center.getId(),
+                center.getName(),
+                center.getAddress(),
+                center.getTel(),
+                center.getWebUrl(),
+                center.getInstagramUrl(),
+                center.getYoutubeUrl(),
+                center.getImgList().stream().map(CenterImgDto::from).collect(Collectors.toList()),
+                center.getOperatingTime().stream().map(OperatingTimeDto::from).collect(Collectors.toList()),
+                center.getFacilities(),
+                center.getCharge().stream().map(ChargeDto::from).collect(Collectors.toList()),
+                center.getChargeImg(),
+                holdInfoList.stream().map(HoldInfoResponseDto::from).collect(Collectors.toList()),
+                center.getHoldInfoImg(),
+                center.getSectorInfo().stream().map(SectorInfoDto::from).collect(Collectors.toList()),
+                isBookmarked,
+                postCount,
+                reviewCount
+        );
+    }
+}
--- src/main/java/coLaon/ClaonBack/post/repository/PostRepository.java
+++ src/main/java/coLaon/ClaonBack/post/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package coLaon.ClaonBack.post.repository;
 
+import coLaon.ClaonBack.center.domain.Center;
 import coLaon.ClaonBack.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,4 +15,25 @@ public interface PostRepository extends JpaRepository<Post, String> {
     Optional<Post> findByIdAndIsDeletedFalse(String id);
     @Query(value = "SELECT p.id FROM TB_POST AS p WHERE p.user_id = :userId", nativeQuery = true)
     List<String> selectPostIdsByUserId(@Param("userId") String userId);
+    @Query(value = "SELECT COUNT(p) " +
+            "FROM ( " +
+            "SELECT p " +
+            "FROM TB_POST AS p " +
+            "JOIN TB_USER AS u " +
+            "ON p.user_id = u.id " +
+            "WHERE p.center_id = :centerId AND p.is_deleted = false " +
+            "AND (u.is_private = false OR p.user_id = :userId) " +
+            "EXCEPT " +
+            "SELECT p " +
+            "FROM TB_POST AS p " +
+            "RIGHT JOIN TB_BLOCK_USER AS bu " +
+            "ON p.user_id = bu.block_user_id " +
+            "WHERE bu.user_id = :userId " +
+            "EXCEPT " +
+            "SELECT p " +
+            "FROM TB_POST AS p " +
+            "RIGHT JOIN TB_BLOCK_USER AS bu " +
+            "ON p.user_id = bu.user_id " +
+            "WHERE bu.block_user_id = :userId) a" , nativeQuery = true)
+    Integer selectCountByCenter(@Param("centerId")String centerId, @Param("userId")String userId);
 }
