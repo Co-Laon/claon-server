935e33708a841d3be8cc0b0a665befb05d3462f3
minas1222 <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>
2022-08-13 00:10:18 +0900

    refac: refactor s3 image upload method for resizing image

--- src/main/java/coLaon/ClaonBack/common/utils/S3Util.java
+++ src/main/java/coLaon/ClaonBack/common/utils/S3Util.java
@@ -4,6 +4,7 @@ import coLaon.ClaonBack.common.exception.ErrorCode;
 import coLaon.ClaonBack.common.exception.InternalServerErrorException;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,10 +12,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDate;
-import java.util.Optional;
+import java.util.Objects;
 import java.util.UUID;
 
 @Slf4j
@@ -22,56 +23,39 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class S3Util {
     private final AmazonS3Client amazonS3Client;
+    private final ImageUtil imageUtil;
 
     @Value("${cloud.aws.s3.bucket}")
     public String bucket;
 
-    public String upload(MultipartFile multipartFile, String dirName) {
-        File uploadFile = convert(multipartFile).orElseThrow(
-                () -> new InternalServerErrorException(
-                        ErrorCode.INTERNAL_SERVER_ERROR,
-                        "파일 전환에 실패했습니다."
-                )
-        );
-        return upload(uploadFile, dirName);
+    public String upload(MultipartFile uploadFile, String dirName) {
+        String fileFormatName = Objects.requireNonNull(uploadFile.getContentType())
+                .substring(uploadFile.getContentType().lastIndexOf("/") + 1);
+        String fileName = dirName + "/" + LocalDate.now() + "/" + UUID.randomUUID() + "." + fileFormatName;
+        MultipartFile resizedFile = imageUtil.resizeImage(uploadFile, fileFormatName, fileName);
+        ObjectMetadata objectMetadata = getObjectMetadata(uploadFile, resizedFile);
+
+        return putS3(resizedFile, fileName, objectMetadata);
     }
 
-    private String upload(File uploadFile, String dirName) {
-        String fileName = dirName + "/" + LocalDate.now() + "/" + UUID.randomUUID() + "." +
-                uploadFile.getName().substring(uploadFile.getName().lastIndexOf(".") + 1);
-        String uploadImageUrl = putS3(uploadFile, fileName);
-        removeNewFile(uploadFile);
-        return uploadImageUrl;
-    }
-
-    private String putS3(File uploadFile, String fileName) {
-        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+    private String putS3(MultipartFile resizedFile, String fileName, ObjectMetadata objectMetadata) {
+        try (InputStream inputStream = resizedFile.getInputStream()) {
+            amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e) {
+            throw new InternalServerErrorException(
+                    ErrorCode.INTERNAL_SERVER_ERROR,
+                    "이미지 업로드를 실패했습니다."
+            );
+        }
         return amazonS3Client.getUrl(bucket, fileName).toString();
     }
 
-    private void removeNewFile(File targetFile) {
-        if (targetFile.delete()) {
-            log.info("File delete success");
-            return;
-        }
-        log.info("File delete fail");
-    }
+    private ObjectMetadata getObjectMetadata(MultipartFile file, MultipartFile resizedFile) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(resizedFile.getSize());
+        objectMetadata.setContentType(file.getContentType());
 
-    private Optional<File> convert(MultipartFile multipartFile) {
-        File convertFile = new File(
-                System.getProperty("user.dir") + "/" + multipartFile.getOriginalFilename()
-        );
-
-        try (FileOutputStream fos = new FileOutputStream(convertFile)) {
-            if (convertFile.createNewFile()) {
-                fos.write(multipartFile.getBytes());
-            } else {
-                return Optional.empty();
-            }
-        } catch (Exception e) {
-            return Optional.empty();
-        }
-
-        return Optional.of(convertFile);
+        return objectMetadata;
     }
 }
