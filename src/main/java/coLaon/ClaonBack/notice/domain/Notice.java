f74a2f3f7725ccb52c1ea45044970827d5cc4e74
ChaeByunghoon <cbh1203@naver.com>
2022-08-27 10:58:54 +0900

    refactor: apply peer review

--- src/main/java/coLaon/ClaonBack/notice/domain/Notice.java
+++ src/main/java/coLaon/ClaonBack/notice/domain/Notice.java
@@ -8,9 +8,11 @@ import lombok.RequiredArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Getter
+@Table(name = "tb_notice")
 @RequiredArgsConstructor
 public class Notice extends BaseEntity {
 
--- src/main/java/coLaon/ClaonBack/notice/dto/NoticeCreateRequestDto.java
+++ src/main/java/coLaon/ClaonBack/notice/dto/NoticeCreateRequestDto.java
@@ -2,9 +2,11 @@ package coLaon.ClaonBack.notice.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class NoticeCreateRequestDto {
 
     private String title;
