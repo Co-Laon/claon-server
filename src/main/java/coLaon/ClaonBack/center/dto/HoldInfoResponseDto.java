ff3fc24b2dbf5a14456b461822276d0983b11685
Minsoo <37828653+als95@users.noreply.github.com>
2022-09-05 15:21:12 +0900

    refac: refactor add crayon url for center (#228)

--- src/main/java/coLaon/ClaonBack/center/dto/HoldInfoResponseDto.java
+++ src/main/java/coLaon/ClaonBack/center/dto/HoldInfoResponseDto.java
@@ -8,24 +8,34 @@ public class HoldInfoResponseDto {
     private String id;
     private String name;
     private String image;
+    private String crayonImage;
 
     private HoldInfoResponseDto(
             String id,
             String name,
-            String image
+            String image,
+            String crayonImage
     ) {
         this.id = id;
         this.name = name;
         this.image = image;
+        this.crayonImage = crayonImage;
     }
 
     public static HoldInfoResponseDto from(
             HoldInfo holdInfo
     ) {
+        String[] holdInfoImageUrl = holdInfo.getImg().split("hold/");
+
+        String crayonImageUrl = "";
+        if (holdInfoImageUrl.length == 2)
+            crayonImageUrl = holdInfoImageUrl[0] + "crayon/" + holdInfoImageUrl[1];
+
         return new HoldInfoResponseDto(
                 holdInfo.getId(),
                 holdInfo.getName(),
-                holdInfo.getImg()
+                holdInfo.getImg(),
+                crayonImageUrl
         );
     }
 }
