02b77843108e57e4e2bab6849a0fa1343c85a122
Minsoo <als95.dev@gmail.com>
2022-09-30 11:49:46 +0900

    refac: refactor cors option in security config

--- src/main/java/coLaon/ClaonBack/config/WebSecurityConfig.java
+++ src/main/java/coLaon/ClaonBack/config/WebSecurityConfig.java
@@ -35,9 +35,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors()
-
-                .and()
+                .httpBasic().disable()
                 .csrf().disable()
 
                 .exceptionHandling()
