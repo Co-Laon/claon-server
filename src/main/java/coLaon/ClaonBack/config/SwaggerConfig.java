13d540639342a13cb0d65fcf2bea09127ab63537
Minsoo <als95.dev@gmail.com>
2022-10-26 09:23:15 +0900

    fix: fix authenticationprincipal not visible in swagger

--- src/main/java/coLaon/ClaonBack/config/SwaggerConfig.java
+++ src/main/java/coLaon/ClaonBack/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package coLaon.ClaonBack.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -18,6 +19,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
+                .ignoredParameterTypes(AuthenticationPrincipal.class)
                 .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("coLaon.ClaonBack"))
