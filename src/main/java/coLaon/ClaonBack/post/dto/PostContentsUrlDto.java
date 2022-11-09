ebdb04b266d1a28e2e34d40249a84cdc0fecfd4e
minas <98951922+minas1222@users.noreply.github.com>
2022-11-09 10:26:00 +0900

    feat: implement delete s3 image for post contents using image path (#301)

---
+++ src/main/java/coLaon/ClaonBack/post/dto/PostContentsUrlDto.java
@@ -0,0 +1,13 @@
+package coLaon.ClaonBack.post.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostContentsUrlDto {
+
+    private String url;
+}
