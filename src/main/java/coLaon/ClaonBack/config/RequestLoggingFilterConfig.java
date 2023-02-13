5ef3e47b57d73eee79c45f933cb185aeb5b8688e
Minsoo <als95.dev@gmail.com>
2023-02-13 20:57:21 +0900

    feat: add info log in interceptor

---
+++ src/main/java/coLaon/ClaonBack/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,20 @@
+package coLaon.ClaonBack.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+    @Bean
+    public CommonsRequestLoggingFilter loggingFilter() {
+        CommonsRequestLoggingFilter filter
+                = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        filter.setAfterMessagePrefix("request : ");
+        return filter;
+    }
+}
