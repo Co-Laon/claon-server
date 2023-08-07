29b71c06067b437cc8b4421c13c107ff97f9747a
als95 <als95.dev@gmail.com>
2023-08-07 17:11:02 +0900

    feat: add modules about each domain

---
+++ center/src/test/java/com/claon/center/CenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claon.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
