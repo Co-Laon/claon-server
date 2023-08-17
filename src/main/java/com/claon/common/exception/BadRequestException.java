772c09765d3d229dad48a158be2e179eb0357ebc
Minsoo <als95.dev@gmail.com>
2023-08-17 17:13:42 +0900

    refac: delete original src file (#367)

--- src/main/java/com/claon/common/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.claon.common.exception;
-
-public class BadRequestException extends BaseRuntimeException{
-    public BadRequestException(ErrorCode errorCode, String message) {
-        super(errorCode, message);
-    }
-}
--- src/main/java/com/claon/common/exception/ConflictExceptionDto.java
+++
@@ -1,7 +0,0 @@
-package com.claon.common.exception;
-
-public class ConflictExceptionDto extends ExceptionDto {
-    public ConflictExceptionDto(String message) {
-        super(ErrorCode.CONFLICT_STATE, message);
-    }
-}
