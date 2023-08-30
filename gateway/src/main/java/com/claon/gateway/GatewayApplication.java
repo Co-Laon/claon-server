5ec9f6ecc4035e1744c356d4769017fe54e87a0f
Minsoo <als95.dev@gmail.com>
2023-08-31 00:25:13 +0900

    feat: add logger for msa (#380)

--- gateway/src/main/java/com/claon/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/claon/gateway/GatewayApplication.java
@@ -2,11 +2,13 @@ package com.claon.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
