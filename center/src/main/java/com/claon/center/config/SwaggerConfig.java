04a5fed7ab0e5f73f5d09c256678ec9c6f284727
als95 <als95.dev@gmail.com>
2023-10-27 17:37:09 +0900

    feat: add swagger for each module

---
+++ center/src/main/java/com/claon/center/config/SwaggerConfig.java
@@ -0,0 +1,66 @@
+package com.claon.center.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.media.IntegerSchema;
+import io.swagger.v3.oas.models.media.ObjectSchema;
+import io.swagger.v3.oas.models.media.UUIDSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
+
+import java.util.List;
+import java.util.Optional;
+
+@OpenAPIDefinition(info = @Info(title = "CLAON Center API", version = "v1"))
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi customTestOpenAPi() {
+        String[] paths = {"/**"};
+
+        return GroupedOpenApi
+                .builder()
+                .group("Center API")
+                .pathsToMatch(paths)
+                .addOperationCustomizer(new GlobalOperationCustomizer())
+                .build();
+    }
+
+    public static class GlobalOperationCustomizer implements OperationCustomizer {
+        @Override
+        public Operation customize(Operation operation, HandlerMethod handlerMethod) {
+            List<Parameter> parameterList = new java.util.ArrayList<>(Optional.ofNullable(operation.getParameters())
+                    .orElseGet(List::of)
+                    .stream()
+                    .map(p -> {
+                        if (p.getName().equals("userInfo")) {
+                            return new Parameter()
+                                    .in(ParameterIn.HEADER.toString())
+                                    .name("X-USER-ID")
+                                    .schema(new UUIDSchema());
+                        }
+                        if (p.getName().equals("pageable")) {
+                            var schema = new ObjectSchema();
+                            schema.addProperty("page", new IntegerSchema());
+                            schema.addProperty("size", new IntegerSchema());
+                            return new Parameter()
+                                    .in(ParameterIn.QUERY.toString())
+                                    .required(true)
+                                    .name("pageable")
+                                    .schema(schema);
+                        }
+                        return p;
+                    })
+                    .toList());
+
+            operation.setParameters(parameterList);
+            return operation;
+        }
+    }
+}
